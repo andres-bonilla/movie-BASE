e51a59fdd487770594961dbc5302a56d3103616a
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-12-05 22:35:30 -0300

    Update of header and footer

--- src/layouts/Foot.jsx
+++ src/layouts/Foot.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 
 import tmdbLogo from "../assets/tmdb_logo.svg";
+import PortfolioLogo from "../assets/portfolio_logo.svg?react";
 
-export const Foot = () => {
+export const Foot = ({ colorMode }) => {
   return (
-    <footer id="foot">
-      <span id="label-tmdb">Powered by:</span>
+    <footer id="foot" className={`${colorMode} fade-in-large`}>
+      <a
+        id="portfolio"
+        className="no-link-style"
+        href="https://andres-bonilla.vercel.app"
+      >
+        <PortfolioLogo id="portfolio-logo" />
+
+        <span id="name">
+          Andrés
+          <br />
+          Bonilla
+          <br />© 2024
+        </span>
+      </a>
+
+      <span id="rights" className="label">
+        All Rights Reserved.
+      </span>
+
       <a id="tmdb" className="no-link-style" href="https://www.themoviedb.org">
+        <span className="label">Powered by:</span>
+
         <img id="tmdb-logo" src={tmdbLogo} alt="TMDB" />
       </a>
     </footer>
