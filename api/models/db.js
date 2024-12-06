10d59794a98ba22590fbf0a3e68186f43fb0b5fa
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-12-06 02:07:18 -0300

    ENV variables config

--- api/models/db.js
+++ api/models/db.js
@@ -1,9 +1,5 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("moviebasedb", null, null, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+const db = new Sequelize(process.env.DB_URL, { dialect: "postgres" });
 
 module.exports = db;
--- api/services/helpers/urlMakers.js
+++ api/services/helpers/urlMakers.js
@@ -1,5 +1,5 @@
 const urlTmdb = "https://api.themoviedb.org/3",
-  apiKey = "api_key=22d3fd94d0f9fd60515cd799770db756",
+  apiKey = "api_key=" + process.env.TMDB_API_KEY,
   apiLang = "language=es-MX",
   extras = "similar,recommendations,credits,videos,watch/providers";
 
