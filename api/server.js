c1814aabef0c55d4e2573528cebebb8ec123906e
Andrés Bonilla <x.andresbonilla@gmail.com>
2024-12-06 03:04:04 -0300

    ENV variables config

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const express = require("express"),
   LocalStrategy = require("passport-local").Strategy;
 
 const app = express(),
-  port = 3001;
+  port = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
