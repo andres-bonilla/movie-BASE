c2c0ab5c593cd5c7b24201a9651676022830a4c5
Andrés Bonilla <x.andresbonilla@gmail.com>
2025-01-11 15:18:42 -0300

    Fixed first loading animation

--- client/src/helpers/getContentByStatus.jsx
+++ client/src/helpers/getContentByStatus.jsx
@@ -6,7 +6,7 @@ import { ErrorMessage } from "../components/utils/ErrorMessage";
 export const getContentByStatus = (type, status, err, isEmpty) => {
   const contentMap = {
     delaying: null,
-    loading: <Loader type={type} />,
+    loading: <Loader type={type === "app" ? "home" : type} />,
     error: <ErrorMessage type={err.type} error={err} />,
     success: null,
     empty: <ErrorMessage type={type === "app" ? "imageLoad" : "dataLoad"} />,
